06ed53ea5ccd288f712c70449a773217110a98f5
Fateh Mammadli <fatehmmmdli@Fatehs-MacBook-Air.local>
2022-06-09 10:25:58 +0400

    live free or die

---
+++ Homework09JUNE/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework09JUNE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+
+            Product product1 = new Journal()
+            {
+                Name = "Wall Street Journal",
+                Company = "Wall Street"
+
+            };
+
+            Product product2 = new Book()
+            {
+                Name = "Senanin maceralari",
+                Genre = "Horror"
+
+            };
+
+            Product product3 = new Journal()
+            {
+                Name = "NY Times",
+                Company = "Wall Street"
+
+            };
+
+            Product product4 = new Book()
+            {
+                Name = " maceralari",
+                Genre = "Horror / Adventure"
+
+            };
+
+             
+           
+        }
+    }
+}
+
